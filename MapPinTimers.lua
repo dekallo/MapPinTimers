2df9d9a5af23b0bd27d97e2565e5d4a89eb36c3b
Nick <ntowle@gmail.com>
2022-10-29 11:33:27 -0400

    scoping

--- MapPinTimers.lua
+++ MapPinTimers.lua
@@ -13,56 +13,60 @@ SuperTrackedFrame.TimeText:SetPoint("TOP", SuperTrackedFrame.Icon, "BOTTOM", 0, 
 local fullAlpha = true
 
 -- override frame alpha to full opacity so the timer is useful
-local oldAlpha = SuperTrackedFrame.GetTargetAlphaBaseValue
-function SuperTrackedFrame:GetTargetAlphaBaseValue()
-  return fullAlpha and 1 or oldAlpha(self)
+do
+  local oldAlpha = SuperTrackedFrame.GetTargetAlphaBaseValue
+  function SuperTrackedFrame:GetTargetAlphaBaseValue()
+    return fullAlpha and 1 or oldAlpha(self)
+  end
 end
 
 -- replaces UpdateDistanceText from Blizzard_QuestNavigation/SuperTrackedFrame.lua
-local throttle = 0
-local lastDistance = nil
-local function UpdateDistanceTextWithTimer(self, elapsed)
-  self.DistanceText:SetShown(not self.isClamped)
+do
+  local throttle = 0
+  local lastDistance = nil
+  local function UpdateDistanceTextWithTimer(self, elapsed)
+    self.DistanceText:SetShown(not self.isClamped)
 
-    if not self.isClamped then
-      local distance = C_Navigation.GetDistance()
+      if not self.isClamped then
+        local distance = C_Navigation.GetDistance()
 
-      throttle = throttle + elapsed
-      if throttle >= .5 then
-        local speed = lastDistance and ((lastDistance - distance) / throttle) or 0
-        lastDistance = distance
+        throttle = throttle + elapsed
+        if throttle >= .5 then
+          local speed = lastDistance and ((lastDistance - distance) / throttle) or 0
+          lastDistance = distance
 
-        if speed > 0 then
-          local time = abs(distance / speed)
-          self.TimeText:SetText(TIMER_MINUTES_DISPLAY:format(floor(time / 60), floor(time % 60)))
-          self.TimeText:SetShown(true)
-        else
-          self.TimeText:SetShown(false)
+          if speed > 0 then
+            local time = abs(distance / speed)
+            self.TimeText:SetText(TIMER_MINUTES_DISPLAY:format(floor(time / 60), floor(time % 60)))
+            self.TimeText:SetShown(true)
+          else
+            self.TimeText:SetShown(false)
+          end
+
+          throttle = 0
         end
 
-        throttle = 0
-      end
-
-      self.DistanceText:SetText(IN_GAME_NAVIGATION_RANGE:format(Round(distance)))
-    else
-      self.TimeText:SetShown(false)
-      lastDistance = nil
+        self.DistanceText:SetText(IN_GAME_NAVIGATION_RANGE:format(Round(distance)))
+      else
+        self.TimeText:SetShown(false)
+        lastDistance = nil
+    end
   end
-end
 
--- replaces OnUpdate from Blizzard_QuestNavigation/SuperTrackedFrame.lua
-local function OnUpdateTimer(self, elapsed)
-  self:CheckInitializeNavigationFrame(false)
-
-  if self.navFrame then
-    self:UpdateClampedState()
-    self:UpdatePosition()
-    self:UpdateArrow()
-
-    -- this replaces the original self:UpdateDistanceText
-    UpdateDistanceTextWithTimer(self, elapsed)
-
-    self:UpdateAlpha()
+  -- replaces OnUpdate from Blizzard_QuestNavigation/SuperTrackedFrame.lua
+  local function OnUpdateTimer(self, elapsed)
+    self:CheckInitializeNavigationFrame(false)
+  
+    if self.navFrame then
+      self:UpdateClampedState()
+      self:UpdatePosition()
+      self:UpdateArrow()
+  
+      -- this replaces the original self:UpdateDistanceText
+      UpdateDistanceTextWithTimer(self, elapsed)
+  
+      self:UpdateAlpha()
+    end
   end
+  SuperTrackedFrame:SetScript("OnUpdate", OnUpdateTimer)
 end
-SuperTrackedFrame:SetScript("OnUpdate", OnUpdateTimer)
